ec0622cb34965746d4c90dba23ec6808f71acc5c
Valtteri Poutanen <valtteri.poutanen@haltu.fi>
2017-11-29 11:48:58 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+class App extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Module Screen</Text>
+      </View>
+    )
+  }
+}
+export default App
