caf5ee04df16f51c5ec708461acbbb028be04383
taturaja <tatu.rajaniemi@haltu.fi>
2017-12-19 12:55:19 +0200

    Export reusable components

--- index.js
+++ index.js
@@ -6,12 +6,17 @@ import {
   createSingleChoiceStep,
   createMultiChoiceStep,
 } from 'open-city-modules/src/steps';
+
 // Modules
 import FeedbackModule from 'open-city-modules/src/modules/Feedback';
 import WebViewModule from 'open-city-modules/src/modules/WebView';
 import { configureFeedback } from 'open-city-modules/src/modules/Feedback/config';
 import EStyleSheet from 'react-native-extended-stylesheet';
 import { type ColorSet } from 'open-city-modules/src/types';
+
+// Components
+import * as Components from 'open-city-modules/src/components';
+
 // eslint-disable-next-line
 import Development from './development'
 
@@ -26,4 +31,5 @@ export {
   initColors,
   SplashScreen, ChoiceView, createSingleChoiceStep, createMultiChoiceStep,
   WebViewModule, FeedbackModule,
+  Components,
 };
